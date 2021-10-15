488935c5da4e174ac60c59f092669a208cf9dad4
Daniel Bamba <djbamba@live.com>
2021-10-15 17:21:57 -0500

    Add profile to exclude from integration testing.

--- src/main/java/com/github/djbamba/gofish/GoFishCli.java
+++ src/main/java/com/github/djbamba/gofish/GoFishCli.java
@@ -7,10 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!test")
 public class GoFishCli implements CommandLineRunner {
   private final Logger log = LoggerFactory.getLogger(GoFishCli.class);
 
